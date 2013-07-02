476b8f15a6614fa26617ded0adb552f003e7a082
Kevin Trehan <kstrehan@gmail.com>
2013-07-02 10:33:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lype::Application.config.secret_token = '2425f4639d9c130dce81a818897e25f736d4af98525a9e57595b2f150fee9f080cf3784b6728d0dbaa69e57a99292aaa584d9c9f8bb76c7ec509f0c77976f5fc'
