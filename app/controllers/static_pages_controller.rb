ef22c3d6ef65e3a53b5396c67282bafec48ff981
Kevin Trehan <kstrehan@gmail.com>
2013-07-11 10:30:54 -0400

    Add FAQ page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,7 @@ class StaticPagesController < ApplicationController
 	def about
 	end
 
+	def faq
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Lype::Application.routes.draw do
   root to: 'static_pages#home'
 
   get '/learn-more', to: 'static_pages#about', as: "about"
+  get '/faq', to: 'static_pages#faq'
 
 end
